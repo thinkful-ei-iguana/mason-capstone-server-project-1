0a732731676abb0d39f0d86cd8e7e5e0cc363e59
Mason Reichbauer <mreichba@mail.usf.edu>
2020-01-08 10:38:04 -0500

    added testDB into config

--- src/config.js
+++ src/config.js
@@ -3,5 +3,6 @@ module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
   DATABASE_URL: process.env.DATABASE_URL || 'postgresql://alert@localhost/live-alert',
+  TEST_DATABASE_URL: process.env.DATABASE_URL || 'postgresql://alert@localhost/live-alert-test',
   JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
 };
